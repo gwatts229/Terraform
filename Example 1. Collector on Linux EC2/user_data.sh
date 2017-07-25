b8814ee5e4d5a69d674936bb255f85d343ec204e
gwatts229 <gwatts229@gmail.com>
2017-07-24 21:06:34 -0600

    Update user_data.sh

--- Example 1. Collector on Linux EC2/user_data.sh
+++ Example 1. Collector on Linux EC2/user_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /etc
-sudo wget "https://raw.githubusercontent.com/gwatts229/Terraform/master/Linux_sources_json_files/sources.json"
+sudo wget "https://raw.githubusercontent.com/gwatts229/Terraform/master/sources.json/Example_1/sources.json"
 cd /opt
 sudo wget ${Sumo_Collector_Download_URL} -O SumoCollector.sh && sudo chmod +x SumoCollector.sh
 sudo ./SumoCollector.sh -q -Vsumo.accessid=${Sumo_Logic_Access_ID} -Vsumo.accesskey=${Sumo_Logic_Access_Key} -VsyncSources=/etc/sources.json -Vephemeral=true
